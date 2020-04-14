01b891aa63ec3650a6d4cf3fb7fd66f1cbf14219
moslem <moslemmajidi3@gmail.com>
2020-04-14 15:49:08 +0430

    add new class for search news and add url search

--- news/urls.py
+++ news/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .views import NewsList
+from .views import NewsList, NewsSearch
 
 urlpatterns = [
     path('', NewsList.as_view()),
-
+    path('search', NewsSearch.as_view())
 ]
--- news/views.py
+++ news/views.py
@@ -1,5 +1,5 @@
-from .services import get_news
-from rest_framework import generics, views
+from .services import get_news, search_news
+from rest_framework import views
 from rest_framework.response import Response
 from .serializers import NewSerializer
 
@@ -10,3 +10,13 @@ class NewsList(views.APIView):
         data = get_news()
         results = NewSerializer(data, many=True).data
         return Response(results)
+
+
+class NewsSearch(views.APIView):
+
+    def get(self, request):
+        if self.request.method == 'GET':
+            query = self.request.GET.get('query', None)
+            data = search_news(query)
+            results = NewSerializer(data, many=True).data
+            return Response(results)
