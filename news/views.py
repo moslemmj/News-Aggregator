61bc22088b0c2560a0546da8d1fd3612c83e2bc3
moslem <moslemmajidi3@gmail.com>
2020-04-14 16:12:55 +0430

    add IsAuthenticatedOrReadOnly permission to News class in view.py

--- news/views.py
+++ news/views.py
@@ -1,10 +1,11 @@
 from .services import get_news, search_news
-from rest_framework import views
+from rest_framework import views, permissions
 from rest_framework.response import Response
 from .serializers import NewSerializer
 
 
 class NewsList(views.APIView):
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
 
     def get(self, request):
         data = get_news()
@@ -13,10 +14,12 @@ class NewsList(views.APIView):
 
 
 class NewsSearch(views.APIView):
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
 
     def get(self, request):
         if self.request.method == 'GET':
             query = self.request.GET.get('query', None)
+            print("######"+query)
             data = search_news(query)
             results = NewSerializer(data, many=True).data
             return Response(results)
